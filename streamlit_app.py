bd160b9bbf7460c380df691dbb5ab8557e368510
aa3581305-alt <aa3581305@gmail.com>
2026-01-25 16:59:20 +0000

    update

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("最初の第一歩が一番大事です")
